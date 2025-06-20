345d2a81a75e1269447dc7f6f3b07c5f88920e55
Davide Vitturini <davide.vitturini@edu.unito.it>
2025-06-20 19:34:51 +0200

    Added MongoDB bridge

---
+++ mongo-rabbit-bridge/src/test/java/com/rabbit/bridge/mongorabbitbridge/MongoRabbitBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbit.bridge.mongorabbitbridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoRabbitBridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
