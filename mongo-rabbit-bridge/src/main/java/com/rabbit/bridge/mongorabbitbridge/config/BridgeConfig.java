08650c0da8b85b02099a0d047e842ff9cd4816c0
Davide Vitturini <davide.vitturini@edu.unito.it>
2025-06-24 23:34:17 +0200

    Mongo Bridge: Added api call to dereference the service id

--- mongo-rabbit-bridge/src/main/java/com/rabbit/bridge/mongorabbitbridge/config/BridgeConfig.java
+++ mongo-rabbit-bridge/src/main/java/com/rabbit/bridge/mongorabbitbridge/config/BridgeConfig.java
@@ -10,14 +10,17 @@ public class BridgeConfig {
     private String emailQueue;
     private String smsQueue;
     private String appointmentCollection;
+    private String serviceApiBaseUrl;
 
     // getters
     public String getEmailQueue()            { return emailQueue; }
     public String getSmsQueue()              { return smsQueue; }
     public String getAppointmentCollection() { return appointmentCollection; }
+    public String getServiceApiBaseUrl()     { return serviceApiBaseUrl; }
 
     // setters
     public void setEmailQueue(String q)            { this.emailQueue = q; }
     public void setSmsQueue(String q)              { this.smsQueue = q; }
     public void setAppointmentCollection(String c) { this.appointmentCollection = c; }
+    public void setServiceApiBaseUrl(String url)   { this.serviceApiBaseUrl = url; }
 }
